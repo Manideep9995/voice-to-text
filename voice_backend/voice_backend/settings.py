9eb538c2fd1a4b8b95cef03ca671c6d022d8c762
mani deep <vmanideep0@gmail.com>
2026-04-04 18:44:23 +0530

    updated

--- voice_backend/voice_backend/settings.py
+++ voice_backend/voice_backend/settings.py
@@ -128,7 +128,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-key'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.netlify.app"
+]
 # INSTALLED APPS
 INSTALLED_APPS = [
     'django.contrib.admin',
